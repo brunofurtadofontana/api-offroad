c3aa2eaf6034686dd65f7278aec1c98f82731657
brunofurtadofontana <brunofurtado86@hotmail.com>
2020-01-18 19:45:48 -0300

    api con

--- index.js
+++ index.js
@@ -21,11 +21,14 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-
-
-server.listen(8080, function () {
+/*---- LISTENING ----*/
+server.listen(process.env.PORT || 8080, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
+
+// server.listen(8080, function () {
+//   console.log('%s listening at %s', server.name, server.url);
+// });
 
 
 //Rotas REST
