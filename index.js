100e38d8199f04683956d5c56478a1933a123dd1
brunofurtadofontana <brunofurtado86@hotmail.com>
2020-01-21 10:22:15 -0300

    api con

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
 /*---- LISTENING ----*/
-server.listen(process.env.PORT || 3000, function() {
+server.listen(process.env.PORT || 8000, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
 
